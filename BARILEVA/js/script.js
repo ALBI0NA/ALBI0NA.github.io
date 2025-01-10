efcd649674f9056d6a05261865c380f2ab85c25b
ALBI0NA <albiona.barileva@umib.net>
2025-01-10 09:36:09 +0100

    update script.js

--- BARILEVA/js/script.js
+++ BARILEVA/js/script.js
@@ -13,8 +13,16 @@ document.addEventListener('DOMContentLoaded', function() {
   });
 
  
-
-; 
+  
+    const urlParams = new URLSearchParams(window.location.search);
+    const name = urlParams.get('name');
+    const email = urlParams.get('email');
+    const message = urlParams.get('message');
+    
+    // Display the submitted data
+    document.getElementById('name').textContent = name;
+    document.getElementById('email').textContent = email;
+    document.getElementById('message').textContent = message;
 
 
 
